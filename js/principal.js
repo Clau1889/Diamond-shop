af8f94ee5aa8e3cfbd35d445bf66f275c765af59
clau1889 <clau.blanco1889@gmail.com>
2018-08-06 21:21:52 -0500

    En este commit se realiza función para acceder a la Api de mercado libre y función para acceder a la data

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,43 @@
 jQuery(document).ready(function($){
+
+    /*---Inicializando Carousel*/
 	$('.carousel').carousel({
         interval: 2000
     });						
 
+    getData();
+    /*---FUNCION AJAX PARA TODOS LOS POKEMONES---*/
+    function getData() {
+        $.ajax({
+            url: 'https://api.mercadolibre.com/sites/MLA/search?q=joyeria',
+            type: 'GET',
+            datatype: 'json',
+            primary_results: 25,
+            limit: 12
+        })
+            .done(function (response) {
+                const getAllData = (response);
+                getDescriptionJewelry(getAllData);
+            })
+            .fail(function () {
+                console.log("Error");
+            })
+    }
 
+    function getDescriptionJewelry(data){
+        console.log(data);
+        let arrayjewelry= [];
+        let searchJewelry = data.results;
+
+        for (var i = 0; i < searchJewelry.length; i++) {
+            let allJewelry = searchJewelry[i];
+            console.log(allJewelry);
+            let photo = allJewelry.thumbnail;
+            console.log(photo);
+            let title = allJewelry.title;
+            console.log(title);
+            let price= allJewelry.price;
+            console.log(price);
+        }
+    }
 });
