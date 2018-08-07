0c0baf6db47bbb4a12c5ee8f21f376fd41b212a8
clau1889 <clau.blanco1889@gmail.com>
2018-08-07 16:33:37 -0500

    En este commit se realiza función para acceder al objeto indiviudal de cada artículo seleccionado

--- js/principal.js
+++ js/principal.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($){
     });						
 
     getData();
-    /*---FUNCION AJAX PARA TODOS LOS POKEMONES---*/
+    /*---FUNCION AJAX PARA TODA LA DATA POR CATEGORIA JEWELRY---*/
     function getData() {
         $.ajax({
             url: 'https://api.mercadolibre.com/sites/MLA/search?q=Jewelry=nacklaces=rings=bracelet',
@@ -23,7 +23,7 @@ jQuery(document).ready(function($){
     }
 
     function getDescriptionJewelry(data){
-        console.log(data);
+        //console.log(data);
         let searchJewelry = data.results;
 
         for (var i = 0; i < searchJewelry.length; i++) {
@@ -35,6 +35,7 @@ jQuery(document).ready(function($){
             //console.log(photo);
             let name = allJewelry.title;
             //console.log(name);
+            let justPrice= allJewelry.price;
             let price= '$ ' + allJewelry.price.toFixed(2) + ' MX';
             //console.log(price);
 
@@ -43,7 +44,7 @@ jQuery(document).ready(function($){
     }
 
     function createTemplate (id, photo, name, price){
-        console.log(id, photo, name, price);
+        // console.log(id, photo, name, price);
 
         const template = '<div class="col-6 col-md-3 col-lg-3 card-box">' +
                             '<div class="card">' +
@@ -51,19 +52,65 @@ jQuery(document).ready(function($){
                                 '<div class="card-body">' +
                                     '<p class="name-item">'+name+'</p>' +
                                     '<span class="price-item text-black font-weight-bold"> '+price+'</span>' +
-                                    '<br>' +
-                                    '<a class="card-title text-center right" href="" id-itemd="'+id+'">more ></a>' +
+                                    '</br>' +
+                                    '<a class="card-title text-center right see-more" href="" id-item="'+id+'">More ></a>' +
                                     '<button class="cart-icon" type="submit"><i class="fas fa-cart-plus"></i></button>'
                                 '</div>' +
                             '</div>' +
                         '</div>'
 
-        
-        const boxPortfolio = $('#box-portfolio');
-        boxPortfolio.append(template);
 
         return template;
     }
 
+    /*---FUNCIÓN PARA ENLAZAR ID DEL ITEM SELECCIONADO AL DAR CLICK EN "MORE"---*/
+    $(document).on('click','.see-more', function(e){
+        e.preventDefault();
+
+        const idItem = $(this).attr('id-item');
+        // console.log(idItem);
+        getDataEachItem(idItem);
+    });
+    
+    /*---FUNCIÓN PARA OBTENER LA DATA INFORMACION DEL ARTÍCULO SEGUN EL ID---*/
+    function getDataEachItem(eachData){
+        $.ajax({
+            url: 'https://api.mercadolibre.com/items/' + `${eachData}`,
+            type: 'GET',
+            datatype: 'json',
+        })
+            .done(function (response) {
+                const eachItem = (response);
+                descriptionItem(eachItem);
+            })
+            .fail(function () {
+                console.log("Error");
+            })
+        }
+    
+    /*---FUNCIÓN PARA OBTENER LA DATA SELECCIONADO---*/
+    function descriptionItem(dataItem){
+        console.log(dataItem);
+
+        let name= dataItem.title;
+        console.log(name);
+        let photo= dataItem.thumbnail;
+        console.log(photo);
+        let allPictures= dataItem.pictures;
+
+        for (var j= 0; j < allPictures.length; j++){
+            let pictures= allPictures[j];
+            console.log(pictures);
+
+            let url= pictures.url;
+            console.log(url);
+        }
+
+        let price= dataItem.price;
+        console.log(price);
+        
+
+        return (name, photo, allPictures, price);
+    }
 
 });
